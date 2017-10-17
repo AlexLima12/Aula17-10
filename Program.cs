eb6475f3f52124d4841abfe39938376c2bec2c64
FIC <alexplima@yahoo.com.br>
2017-10-17 18:19:17 -0200

    Criado pasta da aula 17/10

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula17_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
